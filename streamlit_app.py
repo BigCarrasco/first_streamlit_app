aecce145dbdcbabb5abb416e108851a2d70c807a
Mike Carrasco <105386105+BigCarrasco@users.noreply.github.com>
2023-11-24 10:20:03 -0600

    Update streamlit_app.py
    emoji added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My parents New Healthy diner')
-streamlit.header('Breakfast Menu')
+streamlit.title('🥗My parents New Healthy diner')
+streamlit.header('🍞Breakfast Menu')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥑Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Kale, Spinach & Rocket Smothie')
+streamlit.text('🥣 Hard-Boiled Free-Range Egg')
+  
