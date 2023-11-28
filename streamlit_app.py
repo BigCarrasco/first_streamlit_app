8e7f368cf440ef055c355a8a98a9d255536f0cf3
Mike Carrasco <105386105+BigCarrasco@users.noreply.github.com>
2023-11-28 11:23:46 -0600

    Update streamlit_app.py
    la respuesta de fruityvice se transformo a json para que pueda ser mostrada

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fuit advice header')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response) 
-
+#streamlit.text(fruityvice_response) 
+streamlit.text(fruityvice_response.json())
   # https://firstappapp-ymce4dyf6hir7pa2zz9ikb.streamlit.app/
  
