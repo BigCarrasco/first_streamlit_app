b7ced05fa874b72b2848939b1dc445f4a4a9eaa0
Mike Carrasco <105386105+BigCarrasco@users.noreply.github.com>
2023-11-28 11:30:16 -0600

    Update streamlit_app.py
    #normalizacion de la vista del json con pandas
     en la variable fruityvice_normalized

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,14 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fuit advice header')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response) 
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #just write the data on screen
+
+#normalizacion de la vista del json con pandas
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+
+
+
+
   # https://firstappapp-ymce4dyf6hir7pa2zz9ikb.streamlit.app/
  
