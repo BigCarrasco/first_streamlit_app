1ae9e7a393d33c24c5e69d8189adcbe1ba3756a0
Mike Carrasco <105386105+BigCarrasco@users.noreply.github.com>
2023-11-30 11:08:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,19 +28,16 @@ streamlit.dataframe(fruits_to_show)
 
 ####### lesson 9 #######
 streamlit.header('Fuit advice header')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#se comentaron las linea que tenia la respuesta en crudo y sin normalizar
-###streamlit.text(fruityvice_response) 
-###streamlit.text(fruityvice_response.json()) #just write the data on screen
-
-#creacion de la variable para la normalizacion de la vista del json con pandas
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-#exposicion por streamlit la variable normalizada
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
